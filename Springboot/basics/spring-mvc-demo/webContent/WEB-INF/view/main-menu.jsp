a95e1a9e944e9474833b00e37e70baf41838a70a
yigtkaya <68725704+yigtkaya@users.noreply.github.com>
2021-11-30 16:10:49 +0300

    Add files via upload

---
+++ Springboot/basics/spring-mvc-demo/webContent/WEB-INF/view/main-menu.jsp
@@ -0,0 +1,4 @@
+<DOCTYPE html>
+<html>
+<body><h2>
+title is here</h2></body></html>
---
+++ Springboot/basics/springDIExample/BusinessService/src/main/java/com/example/businessservice/topic/TopicRepository.java
@@ -0,0 +1,8 @@
+package com.example.businessservice.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+
+}
