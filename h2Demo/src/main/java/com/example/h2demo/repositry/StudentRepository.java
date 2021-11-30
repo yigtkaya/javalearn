1b2057d7caf96726e5b87993cd98e6c90d186aab
yigtkaya <68725704+yigtkaya@users.noreply.github.com>
2021-11-30 16:13:00 +0300

    Delete h2Demo directory

--- h2Demo/src/main/java/com/example/h2demo/repositry/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.h2demo.repositry;
-
-import com.example.h2demo.model.Student;
-import org.springframework.data.repository.CrudRepository;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-}
