12890180b63ef692c05d0fc855e4c940b767310a
yigtkaya <68725704+yigtkaya@users.noreply.github.com>
2021-11-30 16:12:15 +0300

    Add files via upload

---
+++ Springboot/basics/guides1/Guides1/src/main/java/com/example/guides1/Value.java
@@ -0,0 +1,25 @@
+package com.example.guides1;
+
+public class Value {
+    private long id;
+    private String quote;
+
+    public Value() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    @Override
+    public String toString() {
+        return "Value{" +
+                "id=" + id +
+                ", quote='" + quote + '\'' +
+                '}';
+    }
+}
