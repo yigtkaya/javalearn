eeae79e779360518a5367a4aa690a5bdbc315ea2
yigtkaya <68725704+yigtkaya@users.noreply.github.com>
2021-11-30 16:13:24 +0300

    Delete spring5webapp directory

--- spring5webapp/src/main/java/kaya/spring/spring5webapp/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package kaya.spring.spring5webapp.repository;
-import kaya.spring.spring5webapp.domain.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-
-}
--- spring5webapp/src/main/java/kaya/spring/spring5webapp/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package kaya.spring.spring5webapp.repository;
-
-import kaya.spring.spring5webapp.domain.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
